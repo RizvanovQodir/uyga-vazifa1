2beb6817db226d9b0985deab13a54bf9e6f49232
Rizvanov Qodir <87949066+RizvanovQodir@users.noreply.github.com>
2022-12-19 11:48:35 +0500

    Add files via upload

---
+++ uyga-vazifa.py
@@ -0,0 +1,12 @@
+# K va S hafrlari mavjud bolsa kattasini kichikka, kichigini kattaga ozgartiradi
+# soz = "O'zbeKisTon"
+# list1 = ['K','S']
+# list2 = ['k','s']
+# for i in soz:
+#     if i in list1:
+#         x = i.lower()
+#     elif i in list2:
+#         x = i.upper()
+#     else:
+#         x = i
+#     print(x, end="")
